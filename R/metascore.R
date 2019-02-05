cdbb23e42391e6b44ede21623ad10344d9041764
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-02-05 09:42:10 +0100

    rename and regroup

---
+++ R/metascore.R
@@ -0,0 +1,49 @@
+get_metascore <- function(.x) { 
+  purrr::map(.x = .x, .f = function(.y) {
+    out <- rvest::html_text(
+      x = rvest::html_node(
+        x = xml2::read_html(x = paste0(.y, "criticreviews")), 
+        css = "div.metascore"
+      )              
+    )
+    return(as.numeric(out)/10)
+  })
+}
+
+add_metascore <- function(
+  file_in = "./data/ratings.csv", 
+  file_out = "./data/ratings_ms.csv", 
+  from = 2016, 
+  locale = readr::locale(encoding = "Windows-1252")
+) {
+  suppressMessages({
+    ratings <- readr::read_csv(file = file_in, locale = locale) %>%
+      dplyr::mutate(
+        YearRated = lubridate::year(`Date Rated`),
+        Genres = purrr::map_chr(.x = Genres, .f = capitalise) %>%
+          gsub("Musical", "Music", .)
+      ) %>%
+      dplyr::filter(YearRated>=!!from)
+        
+    if (file.exists(file_out)) {
+      ratings_old <- readr::read_csv(file = file_out)
+      ratings <- dplyr::filter(.data = ratings, !Const%in%ratings_old[["Const"]])
+    
+      if (nrow(ratings)>0) {
+        ratings <- ratings %>%
+          dplyr::mutate(`Metascore Rating` = get_metascore(URL)) %>% 
+          tidyr::unnest() %>%
+          dplyr::bind_rows(ratings_old) %>%
+          readr::write_csv(path = file_out)
+      }
+    } else {
+      if (nrow(ratings)>0) {
+        ratings <- ratings %>%
+          dplyr::mutate(`Metascore Rating` = get_metascore(URL)) %>%
+          tidyr::unnest() %>%
+          readr::write_csv(path = file_out)
+      }
+    }
+  })
+  return(readr::read_csv(file = file_out))
+}
