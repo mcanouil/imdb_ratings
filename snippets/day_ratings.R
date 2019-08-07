4ddd3954e6e4c30caa1594aada5551df2e709792
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-07 15:37:26 +0000

    check week days distribution accross years

--- snippets/day_ratings.R
+++ snippets/day_ratings.R
@@ -1,24 +1,35 @@
 library(tidyverse)
+library(scales)
 library(lubridate)
 
 
 devtools::source_url('https://github.com/mcanouil/DEV/raw/master/R/theme_black.R')
-theme_set(theme_black(base_size = 12))
+theme_set(theme_black(base_size = 12, base_family = "xkcd"))
 
 
 ratings_day <- read_csv(file = "./data/ratings_ms.csv") %>% 
+  filter(YearRated >= 2017) %>% 
   mutate(day = wday(`Date Rated`, label = TRUE, abbr = FALSE, week_start = 1)) %>% 
   group_by(YearRated, day) %>% 
   summarise(
     n = n(),
     mean = mean(`Your Rating`, na.rm = TRUE)
   ) %>% 
-  ungroup() %>% 
   group_by(YearRated) %>% 
-  mutate(p = n / sum(n))
+  mutate(
+    p = n / sum(n)
+  ) %>% 
+  ungroup()
 
-ggplot(data = ratings_day, mapping = aes(x = day, y = p, fill = factor(YearRated))) +
-  geom_bar(stat = "identity", position = position_dodge2()) +
+ggplot(data = ratings_day, mapping = aes(x = factor(YearRated), y = p, fill = day)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_text(
+    mapping = aes(label = n), 
+    position = position_dodge(width = 0.9), 
+    hjust = -0.1, 
+    angle = 90, 
+    family = "xkcd"
+  ) +
   scale_fill_viridis_d() +
-  scale_y_continuous(labels = percent, expand = expand_scale(mult = c(0, 0.05))) +
+  scale_y_continuous(labels = percent, expand = expand_scale(mult = c(0, 0.15))) +
   labs(x = NULL, y = NULL, fill = "Year")
